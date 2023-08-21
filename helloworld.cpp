76fbef888ef1d0c848a5b86a43eb9d69f06fe32e
unknown <bereketkibret80@gmail.com>
2023-08-21 01:50:44 -0700

    helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  
+ // integer ( whole number)
+ int age = 21;
+ int year = 2023;
+ int days =7;
+  
+  // double (number including decimal)
+double price = 10.99;
+double gpa = 2.5;
+double temperature = 25.1;
+
+//single character
+char grade ='A';
+char initial = 'd';
+char dollarsing = '$';
+
+//boolean (true or false)
+bool student = false;
+bool power = true;
+bool forsale = true;
+
+ // string (objects that represent a sequence of text)
+ std:: string name = "bro";
+ std::string day = "friday";
+ std::string food ="doro wate";
+ std::string address ="954 dz";
+
+
+ std::cout << "hello" << name << '\n';
+std::cout << "you are " << age << "years old";
+
+
+    return 0;
+}
